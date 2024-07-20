5369e2a21f88e0fc020cf732e530fba51e5b4071
julianblane <jbenegaslane@gmail.com>
2024-07-20 16:08:10 +0200

    navigation within same tags combination

---
+++ _plugins/NavigatorTags.rb
@@ -0,0 +1,36 @@
+module Jekyll
+  class NavigatorTags < Generator
+    def generate(site)
+      # Generate a map of all posts grouped by the exact same tags combination
+      tag_map = Hash.new { |h, k| h[k] = [] }
+      site.posts.each do |post|
+        tags = post.data['tags']
+        tag_map[tags] << post
+      end
+
+      tag_map.each do |tags, posts|
+        # Iterate over the posts for each tag combination
+        posts.each do |post|
+          index = posts.index post
+
+          next_in_category = nil
+          previous_in_category = nil
+          if index
+            if index < posts.length - 1
+              next_in_category = posts[index + 1]
+            end
+          	if index > 0
+              previous_in_category = posts[index - 1]
+            end
+          end
+          post.data["next_tags"] = next_in_category unless next_in_category.nil?
+          post.data["previous_tags"] = previous_in_category unless previous_in_category.nil?
+        end
+      end
+
+      site.posts.each do |post|
+        post.data['debug'] = tag_map
+      end
+    end
+  end
+end
